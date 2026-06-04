3811ea09a6ff305fa7974df03c23d78d50ad3b80
Marcel Słotwiński <maslotwi@gmail.com>
2026-06-04 19:53:55 +0200

    add health check endpoint and initialize API server

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/maslotwi/graph-auth/api"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	port := 8080
+	fmt.Printf("Listening on port %d\n", port)
+	api.RunAPIServer(port)
 }
